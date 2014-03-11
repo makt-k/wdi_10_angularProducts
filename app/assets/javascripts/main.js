f274eb4c8921e2f7d0b6326b84645e9dcb907d21
K Mak <karen.mak@stx.oxon.org>
2014-03-11 14:21:26 -0400

    set up angular and js files

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+//= require_self
+//= require_tree ./services/main
+//= require_tree ./filters/main
+//= require_tree ./controllers/main
+//= require_tree ./directives/main
