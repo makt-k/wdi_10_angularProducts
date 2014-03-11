f4f6deb1721725ced599581244ec72becf46e0a7
K Mak <karen.mak@stx.oxon.org>
2014-03-11 14:07:53 -0400

    started new rails project, gemfile bundled

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'angularjs-rails'
+gem 'active_model_serializers'
+gem 'quiet_assets'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularProducts::Application.config.secret_key_base = '6831c7cb3425e309750cf48d2ae5153e2507a8decb2cb831d0153dba1dcd72eff01246c185ae051e7d0c1523d584150a0ba6c4fd58f67ba95e3cc760134747f9'
